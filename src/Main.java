bfece9b91e6180cb04cbea90b36b5df2bfb7ce4c
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-09-29 16:06:57 -0300

    Primeiro Projeto em Java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+void main() {
+    IO.println("Bem-vindo ao Orbit Stream!!");
+    IO.println("Filme: Scarface");
+
+    int anoDeLancamento = 1984;
+    IO.println("Ano de Lançamento: " + anoDeLancamento);
+    boolean incluidoNoPlano = true;
+    double notaDofilme = 8.3;
+    String nomeDoFilme = "Scarface";
+
+}
