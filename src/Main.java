450650656d01e8d2d4187121805866fc83d3c0fe
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-10-02 14:18:59 -0300

    updates

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,20 @@ void main() {
     System.out.println(media);
     String nomeDoFilme = "Scarface";
     String nomeDoDiretor = "Diretor";
-    String sinopse;
+    String sinopse = """
+            Após receber residência permanente nos Estados Unidos
+            em troca do assassinato de um oficial do governo cubano,
+             Tony Montana se torna o chefe do tráfico de drogas em Miami.
+             Matando qualquer um que entre em seu caminho,
+             Tony eventualmente se torna o maior traficante da Flórida,
+             controlando quase toda a cocaína que entra em Miami.
+             Porém, a pressão da polícia,
+             as guerras com cartéis colombianos e
+             sua própria paranoia servem para alimentar
+             as chamas de sua eventual queda.""";
+
+    System.out.println(nomeDoFilme + "\n Ano de Lançamento: " + anoDeLancamento + "\n" + sinopse);
+
+    int classificacao;
+    classificacao = (int) (media / 2);
 }
